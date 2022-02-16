6e26728627d740e5d36c42637401fbf2d4554f68
Jason Han <jason.han@jpl.nasa.gov>
2022-02-16 11:03:18 -0800

    Adding missed object

---
+++ src/main/kotlin/org/openmbee/mms5/plugins/Response.kt
@@ -0,0 +1,18 @@
+package org.openmbee.mms5.plugins
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.jackson.*
+
+
+fun Application.configureContentNegotiation() {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+}
+
+class Response {
+}
