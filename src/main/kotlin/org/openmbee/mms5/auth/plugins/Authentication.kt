5e0d93872606f33f5270fb62a21243cefb396f80
Jason Han <jason.han@jpl.nasa.gov>
2022-07-07 16:31:00 -0700

    Add a regex filter in the sparql query

--- src/main/kotlin/org/openmbee/mms5/auth/plugins/Authentication.kt
+++ src/main/kotlin/org/openmbee/mms5/auth/plugins/Authentication.kt
@@ -48,6 +48,7 @@ fun Application.configureAuthentication() {
                             ?group a mms:Group ;
                             mms:id ?groupId ;
                             .
+                            filter regex(?groupId, "$ldapConfigValues.groupNamespace", "i")
                         }
                     """.trimIndent()
 
